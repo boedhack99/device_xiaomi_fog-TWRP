e8343da6d524b05982028802389ec311428fbd59
infinixs5lite <kelvinchinedu97@gmail.com>
2023-06-04 00:52:45 +0100

    update

--- device.mk
+++ device.mk
@@ -123,6 +123,8 @@ TW_BUILD_VERSION := Build by CHINEDU 09039121780
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental"
 
+# Torch
+PB_TORCH_PATH := "/sys/class/leds/led:torch_0"
 
 # Vibrator
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
