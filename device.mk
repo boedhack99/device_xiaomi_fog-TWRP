bb6ce09bca0c8375d9ec8233dcc61e803d084f77
infinixs5lite <kelvinchinedu97@gmail.com>
2023-06-06 08:54:51 +0100

    update

--- device.mk
+++ device.mk
@@ -119,10 +119,6 @@ TW_NO_SCREEN_BLANK := true
 TW_EXCLUDE_APEX := true
 TW_FRAMERATE := 60
 TW_BUILD_VERSION := Build by CHINEDU 09039121780 uttah
-
-#Properties
-TW_OVERRIDE_SYSTEM_PROPS := \
-#   "ro.bootimage.build.date.utc=ro.build.date.utc;ro.build.date.utc;ro.odm.build.date.utc=ro.build.date.utc;ro.product.build.date.utc=ro.build.date.utc;ro.system.build.date.utc=ro.build.date.utc;ro.system_ext.build.date.utc=ro.build.date.utc;ro.vendor.build.date.utc=ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.name=ro.product.system.name"
     
 # Vibrator
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
